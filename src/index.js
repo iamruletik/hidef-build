c3ab2687850a3bdb2f19b15d489d23697d6a188b
Alexander Ruletik <iam@ruletik.org>
2026-05-01 09:03:03 +0500

    Testing scripts from webflow

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Hello, Hidef! This is the main entry point of the application.")
+console.log("LOCAL DEVELOPMENT")
+
+//Image fetch for the artist
+document.addEventListener("DOMContentLoaded", (event) => {
+
+  let rosterItems = document.querySelectorAll('.roster-list-table-content-item')
+  let rosterFloatingImage = document.querySelector('.roster-floating-image')
+
+  rosterItems.forEach((artist) => {
+
+    let artistCurrentImage = artist.querySelector('.roster-list-table-content-item-image')
+
+    artist.addEventListener('mouseenter', (e) => {
+      rosterFloatingImage.style.opacity = 1
+      rosterFloatingImage.src = artistCurrentImage.src
+    }, true)
+
+  })
+
+});
