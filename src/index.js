472ebaf5804cdba08b3ff7cd7e7fb9a2d253af4b
Alexander Ruletik <iam@ruletik.org>
2026-05-01 10:04:18 +0500

    Barba import

--- src/index.js
+++ src/index.js
@@ -1,26 +1,43 @@
-console.log("LOCAL DEVELOPMENT")
+import barba from '@barba/core'
 
 if (LOCAL !== true) {
-    console.log("LOCAL NOT DETECTED")
 
+    
+
+    //MAKE SURE THAT ONLY ONE SCRIPT WORKS BY SETTING LOCAL TO TRUE
     LOCAL = true
 
-    //Image fetch for the artist
 
-    let rosterItems = document.querySelectorAll('.roster-list-table-content-item')
-    let rosterFloatingImage = document.querySelector('.roster-floating-image')
+    barba.init({
+    views: [{
+        namespace: 'roster',
+        beforeEnter(data) {
+            //Image fetch for the artist
+            let rosterItems = document.querySelectorAll('.roster-list-table-content-item')
+            let rosterFloatingImage = document.querySelector('.roster-floating-image')
 
-    console.log(rosterItems)
+            console.log(rosterItems)
 
-    rosterItems.forEach((artist) => {
+            rosterItems.forEach((artist) => {
 
-    let artistCurrentImage = artist.querySelector('.roster-list-table-content-item-image')
+            let artistCurrentImage = artist.querySelector('.roster-list-table-content-item-image')
 
-    artist.addEventListener('mouseenter', (e) => {
-        rosterFloatingImage.style.opacity = 1
-        rosterFloatingImage.src = artistCurrentImage.src
-    }, true)
+            artist.addEventListener('mouseenter', (e) => {
+                rosterFloatingImage.style.opacity = 1
+                rosterFloatingImage.src = artistCurrentImage.src
+            }, true)
 
-    })
+            })
+        }
+    }, {
+        namespace: 'contact',
+        beforeEnter(data) {
+        // do something before entering the `contact` namespace
+        }
+    }]
+    });
+
+
+
 
 }
